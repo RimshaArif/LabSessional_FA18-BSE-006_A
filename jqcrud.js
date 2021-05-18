a9ced4553f2816c5a4958084da72510d4dad7f4e
RimshaArif <rimshaarif723@gmail.com>
2021-05-18 11:41:23 -0700

    LabSessional

--- jqcrud.js
+++ jqcrud.js
@@ -86,7 +86,7 @@ function loadRecipies() {
       for (var i = 0; i < response.length; i++) {
         var rec = response[i];
         recipes.append(
-          `<div class="recipe" data-id="${rec.id}"><h3>${rec.name}</h3><p><button class=" btn-sm float-right">Delete</button><button class=" btn-sm float-right">Edit</button> ${rec.email}</p></div>`
+          `<div class="recipe" data-id="${rec.id}"><h3>${rec.name}</h3><p><button class="btn btn-danger" >Delete</button><button class="btn btn-warning">Edit</button> ${rec.email}</p></div>`
         );
         // recipes.append("<div><h3>" + rec.title + "</h3></div>");
       }
